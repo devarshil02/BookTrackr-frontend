48b58ef6ef046b93a91cea479b33f8c06e56d849
devarshil02 <arshildudhat@gmail.com>
2024-09-21 20:05:50 +0530

    solve the store error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
-import store from './Redux/Store.js'
+import store from './Redux/Store.js';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
