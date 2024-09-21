d258905779eea56ef5c333c08c71ca3501d38359
devarshil02 <arshildudhat@gmail.com>
2024-09-21 23:10:47 +0530

    chenge some code in AuthSlice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import AuthReducer from '../Redux/slices/authSlice';
+import AuthReducer from './slices/authSlice';
 
 export const store = configureStore({
   reducer: {
